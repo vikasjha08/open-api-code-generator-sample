38440466c1e974cc491d6442ca07342076cef520
vikasjha2908 <vikasjha2908@gmail.com>
2024-12-28 01:11:32 +0530

    initial commit

---
+++ src/main/java/com/project/SwaggerCodeGenerator/SwaggerCodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.project.SwaggerCodeGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerCodeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerCodeGeneratorApplication.class, args);
+	}
+
+}
