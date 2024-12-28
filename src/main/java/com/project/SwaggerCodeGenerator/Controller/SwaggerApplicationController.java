baad4e5745ea1a5bec6d7a200aff29b9d6bc584d
vikasjha2908 <vikasjha2908@gmail.com>
2024-12-28 23:16:30 +0530

    initial commit

---
+++ src/main/java/com/project/SwaggerCodeGenerator/Controller/SwaggerApplicationController.java
@@ -0,0 +1,7 @@
+package com.project.SwaggerCodeGenerator.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SwaggerApplicationController extends org.openapitools.configuration.HomeController {
+}
